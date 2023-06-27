e0c57147fd1795566d66b1de489ed28899737174
Nobel Yoo <fizzyinc1@gmail.com>
2023-06-26 23:55:17 -0700

    fix add argument keyword ar

--- tvac/cli.py
+++ tvac/cli.py
@@ -3,7 +3,7 @@ from .client import VariantAnnotationClient
 
 def cli_annotate_variants():
     parser = argparse.ArgumentParser(prog="tvac-annotate-variants", description="Command line tool for annotating variants provided in a filename")
-    parser.add_argument('filename', description="Name of file with variants seperated by newlines")
+    parser.add_argument('filename', help="Name of file with variants seperated by newlines")
     args = parser.parse_args()
     c = VariantAnnotationClient()
     annotations = c.annotate_file(args.filename)
